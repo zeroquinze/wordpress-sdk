dea7f244123232e267596e3cdea5713eb754f0ca
Thawan Keane <thawankeane+dev@hotmail.com>
2019-05-23 14:15:28 -0300

    chore: added prettier configuration

--- lib/index.ts
+++ lib/index.ts
@@ -1,20 +1,20 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosInstance } from 'axios'
 
-import { WordpressOptions, Post } from "./types";
+import { WordpressOptions, Post } from './types'
 
 class Wordpress {
-  private http: AxiosInstance;
+  private http: AxiosInstance
 
-  private constructor(options: WordpressOptions) {
+  private constructor (options: WordpressOptions) {
     this.http = axios.create({
       baseURL: options.url
-    });
+    })
   }
 
-  public async allPosts(): Promise<Post[]> {
-    const response = await this.http.get("/posts");
-    return response.data;
+  public async allPosts (): Promise<Post[]> {
+    const response = await this.http.get('/posts')
+    return response.data
   }
 }
 
-export default Wordpress;
+export default Wordpress
